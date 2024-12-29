90e6804f3a404a0faafd25bb837b487dbdb857af
junnjiee <junnjiee16@gmail.com>
2024-12-29 16:40:13 +0800

    add shadcn charts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        <div className="px-4">{children}</div>
+        <div className="px-12">{children}</div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,17 @@
 
 import { useState } from "react";
 import { DCAInfoForm } from "./components/DCAInfoForm";
+import { DataCard } from "./components/DataCard";
 
 export default function Home() {
   return (
     <>
       <div className="my-5">
         <DCAInfoForm />
+      </div>
+      <div className="border-b px-4 py-3 space-y-0.5">
+        <div className="text-2xl">Apple Inc.</div>
+        <div className="text-xs">AAPL &bull; NASDAQ</div>
       </div>
     </>
   );
