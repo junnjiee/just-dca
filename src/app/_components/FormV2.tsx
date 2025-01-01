29a457a57740df145f7dccbf668be6f32b358151
junnjiee <junnjiee16@gmail.com>
2025-01-01 17:18:11 +0800

    workable form

--- src/app/_components/FormV2.tsx
+++ src/app/_components/FormV2.tsx
@@ -4,12 +4,19 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 
 const formSchema = z.object({
-  ticker: z.string(),
-  amount: z.number(),
-  startDate: z.string().date(),
-  endDate: z.string().date(),
+  ticker: z.string().toUpperCase().nonempty("Enter a ticker"),
+  contri: z
+    .number({
+      required_error: "Enter a number",
+      invalid_type_error: "Enter a number",
+    })
+    .positive(),
+  startDate: z.string().date().nonempty(),
+  endDate: z.string().date().nonempty(),
 });
 
 type FormProps = z.infer<typeof formSchema>;
@@ -23,7 +30,7 @@ export function FormV2() {
     resolver: zodResolver(formSchema),
     defaultValues: {
       ticker: "AAPL",
-      amount: 50,
+      contri: 50,
       startDate: "2024-01-01",
       endDate: "2024-12-01",
     },
@@ -34,12 +41,48 @@ export function FormV2() {
   }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
-        <label htmlFor="ticker">ticker</label>
+        <Label htmlFor="ticker">Ticker</Label>
         <Input id="ticker" placeholder="Ticker" {...register("ticker")} />
         {errors.ticker && <p>{errors.ticker.message}</p>}
       </div>
+
+      <div>
+        <Label htmlFor="contribution">Contribution</Label>
+        <Input
+          id="contribution"
+          type="number"
+          placeholder="USD"
+          step=".01"
+          {...register("contri", {
+            valueAsNumber: true,
+          })}
+        />
+        {errors.contri && <p>{errors.contri.message}</p>}
+      </div>
+
+      <div>
+        <Label htmlFor="startDate">Start Date</Label>
+        <Input
+          id="startDate"
+          type="date"
+          {...register("startDate", { required: true })}
+        />
+        {errors.startDate && <p>{errors.startDate.message}</p>}
+      </div>
+
+      <div>
+        <Label htmlFor="endDate">End Date</Label>
+        <Input
+          id="endDate"
+          type="date"
+          {...register("endDate", { required: true })}
+        />
+        {errors.endDate && <p>{errors.endDate.message}</p>}
+      </div>
+
+      <Button type="submit">Generate</Button>
     </form>
   );
 }
