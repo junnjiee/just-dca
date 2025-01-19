05da8ccbeda581fb7ca2ce10d3ae292928116e9b
junnjiee <junnjiee16@gmail.com>
2025-01-19 14:25:31 +0800

    fix drawer download button unclickable

--- website/src/components/generic/data-table.tsx
+++ website/src/components/generic/data-table.tsx
@@ -284,7 +284,7 @@ function ExportCsvDrawer({ exportToCsv, className }: ExportCsvComponentProps) {
           </DrawerDescription>
         </DrawerHeader>
         <DrawerFooter>
-          <DrawerClose>
+          <DrawerClose asChild>
             <Button onClick={() => exportToCsv()}>Download</Button>
           </DrawerClose>
         </DrawerFooter>
